42756b48ee5e67371f3c63e09ef8f9d4e7194205
JovanaJelisavcic <jelisavcicjovana@gmail.com>
2017-04-05 01:24:49 +0200

    Add files via upload

---
+++ HCI 2.3bv/HCI 2.3bv/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI_2._3bv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void kreiraj_spomenik(object sender, RoutedEventArgs e)
+        {
+           Window1 win1 = new Window1();
+            win1.Show();
+            
+        }
+
+
+        private void dodaj_etiketu(object sender, RoutedEventArgs e)
+        {
+            Window3 win3 = new Window3();
+            win3.Show();
+            
+
+        }
+
+        private void kreiraj_tip(object sender, RoutedEventArgs e)
+        {
+            Window2 win3 = new Window2();
+            win3.Show();
+            
+        }
+    }
+}
---
+++ HCI 2.3bv/HCI 2.3bv/Window2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_2._3bv
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void ponisti_tip(object sender, RoutedEventArgs e)
+        {
+           
+            
+            this.Close();
+        }
+
+       
+        private void sacuvaj_tip(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sačuvan tip");
+            this.Close();
+        }
+
+
+    }
+}
