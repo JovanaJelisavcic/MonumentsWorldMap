909a7ad15b860ab7bc3c42b7f6539ad5f11e51c2
JovanaJelisavcic <jelisavcicjovana@gmail.com>
2017-04-05 09:55:36 +0200

    Add files via upload

---
+++ HCI-master/HCI 2.3bv/HCI 2.3bv/Window1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_2._3bv
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public int id { get; set; }
+
+        public Window1()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+       
+        private void etiketa_iz_spomenika(object sender, RoutedEventArgs e)
+        {
+            Window3 win1 = new Window3();
+            win1.Show();
+            
+        }
+
+        private void ponisti_spomenik(object sender, RoutedEventArgs e)
+        {
+          
+            this.Close();
+        }
+
+        private void sacuvaj_spomenik(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sačuvan spomenik");
+            this.Close();
+        }
+
+       
+
+
+        
+    }
+}
---
+++ HCI-master/HCI 2.3bv/HCI 2.3bv/Window2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_2._3bv
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public int id { get; set; }
+
+        public Window2()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ponisti_tip(object sender, RoutedEventArgs e)
+        {
+           
+            
+            this.Close();
+        }
+
+       
+        private void sacuvaj_tip(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sačuvan tip");
+            this.Close();
+        }
+
+
+    }
+}
